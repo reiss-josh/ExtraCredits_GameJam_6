9a1b27d5fad6491648a66c49e3b5b8310fca83c2
reiss-josh <reiss.joshuae@gmail.com>
2020-08-21 19:36:33 -0700

    Added some robot variaton

---
+++ Assets/Scripts/CustomCursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.isFocused)
+        {
+            Cursor.visible = false;
+            Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = cursorPos;
+        }
+        if (Input.GetButton("Jump"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
